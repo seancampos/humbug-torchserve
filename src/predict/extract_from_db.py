ae81c837bb372574197f41fc76970b2735b74ac0
Sean Campos <sean.m.campos@gmail.com>
2023-02-18 11:19:19 -0800

    Database extractor works with date list instead of range.

--- src/predict/extract_from_db.py
+++ src/predict/extract_from_db.py
@@ -12,32 +12,42 @@ if __name__ == "__main__":
                         prog = 'MED/MSC Mozzware Recording Extractor',
                         description = 'Copy recordings for a UUID list in a date range into a working directory',
                         )
-    parser.add_argument("uuids", help="CSV file containing UUIDs.  Format should be 1 column with header")
-    parser.add_argument("start", help="Start of date range YYYY-MM-DD")
-    parser.add_argument("end", help="End of date range YYYY-MM-DD")
+    parser.add_argument("uuids", help="CSV file containing UUIDs.  Format is 1 column with header")
+    parser.add_argument("dates", help="CSV file containing recordings dates. Format is 1 column with header and YYYY-MM-DD formated dates")
     parser.add_argument("mozzDir", help="Mount point for MozzWear Recordings (e.g. /dbmount/MozzWear)")
     parser.add_argument()
     args = parser.parse_args()
 
     uuid_file = args.uuids
-    start_datetime = datetime.datetime.strptime(args.start, r"%Y-%m-%d")
-    end_datetime = datetime.datetime.strptime(args.end, r"%Y-%m-%d") + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
+    dates_file = args.dates
+    # start_datetime = datetime.datetime.strptime(args.start, r"%Y-%m-%d")
+    # end_datetime = datetime.datetime.strptime(args.end, r"%Y-%m-%d") + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
 
     uuid_list = pd.read_csv(uuid_file).iloc[:,0].tolist()
+    dates_list = pd.read_csv(dates_file.iloc[:,0]).tolist()
+    dates_list.sort_values(dates_list.columns[0], inplace=True)
+
+    start_of_dates = dates_list.iloc[:,0].apply(lambda x: datetime.datetime.strptime(x, r"%Y-%m-%d"))
+    end_of_dates = dates_list.iloc[:,0].apply(lambda x: datetime.datetime.strptime(x, r"%Y-%m-%d") + datetime.timedelta(days=1) - datetime.timedelta(seconds=1))
 
     # connect to database
     client = MongoClient('mongodb://humbug.ac.uk/')
     db = client['backend_upload']
     recordings = db['reports']
 
-    myquery = {"uuid": {"$in": uuid_list}, "datetime_recorded": {"$gt": start_datetime, "$lt": end_datetime}}
-    mydoc = recordings.find(myquery)
+    recordings_df_list = []
+    for start_datetime, end_datetime in zip(start_dates, end_dates):
+        myquery = {"uuid": {"$in": uuid_list}, "datetime_recorded": {"$gt": start_datetime, "$lt": end_datetime}}
+        mydoc = recordings.find(myquery)
 
-    recording_df = pd.DataFrame(list(mydoc))
+        recording_df = pd.DataFrame(list(mydoc))
 
-    recording_df["current_path"] = recording_df['path'].str.replace('/data/MozzWear', args.mozzDir)
+        recording_df["current_path"] = recording_df['path'].str.replace('/data/MozzWear', args.mozzDir)
 
-    recording_df.to_csv(f"MED_Task_{start_datetime.date()}_to_{end_datetime.date()}.csv", index=False)
+        recordings_df_list.append(recording_df)
+
+    pd.concat(recordings_df_list, ignore_index=True)\
+        .to_csv(f"MED_Task_{dates_list.iloc[0][dates_list.columns[0]]}_to_{dates_list.iloc[-1][dates_list.columns[0]]}.csv", index=False)
 
 
 
